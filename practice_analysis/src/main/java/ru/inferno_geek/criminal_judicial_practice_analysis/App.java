35b3ec173d836c192858256da7086599b61096dc
Inferno <mishaad051@gmail.com>
2024-03-04 15:14:07 +0300

    init, doc: add technical specification

---
+++ practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/App.java
@@ -0,0 +1,13 @@
+package ru.inferno_geek.criminal_judicial_practice_analysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ practice_analysis/src/test/java/ru/inferno_geek/criminal_judicial_practice_analysis/AppTest.java
@@ -0,0 +1,20 @@
+package ru.inferno_geek.criminal_judicial_practice_analysis;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
