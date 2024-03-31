6f2ca62b1a30967319f524a12e7ff6afaf10a578
Inferno <mishaad051@gmail.com>
2024-03-31 17:01:10 +0300

    feat: refined the Case class, might take a while

--- practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/Case.java
+++ practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/Case.java
@@ -10,13 +10,28 @@ import lombok.Setter;
 @Setter 
 @NoArgsConstructor
 public class Case {
+    enum Status {
+        OPEN,
+        CLOSED,
+        APPEAL,
+        CASSATION
+    }
 
     private String caseNumber;
     private String assignedCourt;
+    // TODO: ponder if this could be replaced with something else.
+    // ArrayList-ing charges would make it annoying to work with.
     private ArrayList<String> chargesAssigned;
 
-    // TODO: replace with enum, probably
-    private String caseStatus;                       
+    // TODO: ponder if enum is useful
+    private Status caseStatus;
+
+    public Case(String number, String court, Status status) {
+        caseNumber = number;
+        assignedCourt = court;
+        caseStatus = status;        
+    } 
+      
     
 
 }
