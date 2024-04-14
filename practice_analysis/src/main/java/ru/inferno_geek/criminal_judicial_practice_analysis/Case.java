f013f0b6526261bf630d7df8a51b442cb7a1a355
Inferno <mishaad051@gmail.com>
2024-04-14 10:57:17 +0300

    docs: move specs into a folder feat: add enums to deal with cases

--- practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/Case.java
+++ practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/Case.java
@@ -1,32 +1,24 @@
 package ru.inferno_geek.criminal_judicial_practice_analysis;
 
+import java.util.EnumSet;
 import java.util.TreeSet;
 
+import lombok.Data;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter 
-@NoArgsConstructor
+@Data
 public class Case {
     private String caseNumber;
     private String assignedCourt;
-    // TODO: ponder if this could be replaced with something else.
-    // ArrayList-ing charges would make it annoying to work with.
-    // Maybe, do a sorted set of enums? 
+    // Yes, I have done an enum with custom Comparator;
+    // TODO: Are there any better ways?
     private TreeSet<Charges> assignedCharges = new TreeSet<Charges>();
-
-    // TODO: ponder if enum is useful
     private Status caseStatus;
-
+    
+    
     public Case(String number, String court, Status status) {
         caseNumber = number;
         assignedCourt = court;
         caseStatus = status;        
     } 
-      
     
-
 }
