fb1a93b1e58a82b3eecd8fc9799972dece34b112
Inferno_geek <36582674+Infernogeek1@users.noreply.github.com>
2024-06-04 08:51:47 +0300

    feat!: added document parsers

---
+++ practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/DocxParser.java
@@ -0,0 +1,49 @@
+package ru.inferno_geek.criminal_judicial_practice_analysis;
+
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.lang.RuntimeException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.ooxml.POIXMLException;
+public class DocxParser {
+
+    public void parseDocx(Path path) throws IOException, RuntimeException, POIXMLException{
+        
+        InputStream docxFile = null;
+        XWPFDocument doc = null;
+        XWPFWordExtractor extractor = null;
+        FileOutputStream out = null;
+        Path newTxt = null;
+
+        try 
+        {
+            docxFile = new FileInputStream(path.toString());
+            doc = new XWPFDocument(docxFile);
+            extractor = new XWPFWordExtractor(doc);
+            String text = extractor.getText();
+            System.out.println(text);
+            newTxt = Files.createFile(path.resolveSibling(path.getFileName() + ".txt"));
+            out = new FileOutputStream(newTxt.toFile());
+            out.write(text.getBytes());
+        } 
+
+        catch (IOException | RuntimeException e)
+        {
+            e.printStackTrace();
+        }
+
+        finally 
+        {
+            docxFile.close();
+            doc.close();
+            extractor.close();
+            out.close();
+        }
+    }
+}
---
+++ practice_analysis/src/main/java/ru/inferno_geek/criminal_judicial_practice_analysis/PDFParser.java
@@ -0,0 +1,23 @@
+package ru.inferno_geek.criminal_judicial_practice_analysis;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+public class PDFParser {
+
+    public void parsePDF(Path path) throws IOException {
+        File file = new File(path.toString());
+        PDDocument document = PDDocument.load(file);
+        PDFTextStripper pdfTextStripper = new PDFTextStripper();
+        String text = pdfTextStripper.getText(document);
+        Path newTxt = Files.createFile(path.resolveSibling(path.getFileName() + ".txt"));
+        Files.write(newTxt, text.getBytes());
+        document.close();
+    }
+}
+
